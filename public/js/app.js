226a6fd92e54bfd1b450b12b9b17b148e3df67d8
Adam Albrecht <adam.albrecht@gmail.com>
2013-10-16 20:39:43 -0700

    Updated header to be collapsable on mobile

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,7 @@
-var contactLinks = document.getElementsByClassName('contact-link');  
-if (contactLinks && contactLinks.length > 0) {
-  for (var i=0; i<contactLinks.length; i++) {
-    contactLinks[i].setAttribute('href', 'mail' + 'to' + ':adam' + '.' + 'albrecht' + '@' + 'gmail' + '.com');
-  }
-}
+$('.contact-link').each(function(i, link) {
+  $(link).prop('href', 'mail' + 'to' + ':adam' + '.' + 'albrecht' + '@' + 'gmail' + '.com');
+});
+$('.mobile-toggle').on('click', function(e) {
+  e.preventDefault();
+  $('.toggle-content').toggleClass('open');
+});
